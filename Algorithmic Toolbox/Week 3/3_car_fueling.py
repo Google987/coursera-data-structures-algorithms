a02fb22166168d9ba7f6f8f2b9a7606dcdd3a212
Google <30652896+Google987@users.noreply.github.com>
2022-11-05 10:29:12 +0530

    added car fueling answer

---
+++ Algorithmic Toolbox/Week 3/3_car_fueling.py
@@ -0,0 +1,25 @@
+# Problem details: 
+# (Please check page no. 54) https://alexanderskulikov.github.io/files/toolbox_statements.pdf
+
+
+from sys import stdin
+
+
+def min_refills(distance, tank, stops):
+    last_stop_index = 0
+    distance_covered = 0
+    no_of_refills = 0
+    while distance_covered + tank < distance:
+        prev_stop = last_stop_index
+        while last_stop_index < len(stops) and stops[last_stop_index] <= distance_covered + tank:
+            last_stop_index += 1
+        if prev_stop == last_stop_index: return -1
+        distance_covered = stops[last_stop_index - 1]
+        no_of_refills += 1
+
+    return no_of_refills
+
+
+if __name__ == '__main__':
+    d, m, _, *stops = map(int, stdin.read().split())
+    print(min_refills(d, m, stops))
